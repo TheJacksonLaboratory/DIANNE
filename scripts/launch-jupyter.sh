3105617d5f24addf4f94924942571ab077611e6e
Sergii Domanskyi <Sergii.Domanskyi@jax.org>
2026-04-09 08:34:22 -0400

    fix launch script

--- scripts/launch-jupyter.sh
+++ scripts/launch-jupyter.sh
@@ -1,6 +1,8 @@
+# Clear err_jupyter.err before starting if it exists
+[ -f err_jupyter.err ] && > err_jupyter.err
 sbatch run-jupyter-notebook.sb
 
-for i in $(seq 1 10); do
+for i in $(seq 1 60); do
     printf "\r%2ds " "$i"
     sleep 1
     url=$(cat err_jupyter.err 2>/dev/null | grep "http" | grep -v "]" | grep -v "127" | tail -1)
