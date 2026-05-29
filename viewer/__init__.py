d4edd5192baf7eca0a89e5f0226b6ba3473dcfd3
Sergii Domanskyi <Sergii.Domanskyi@jax.org>
2026-05-29 19:41:19 -0400

    make MIQ viewer

--- viewer/__init__.py
+++ viewer/__init__.py
@@ -1,17 +1,4 @@
 from .viewer import create_viewer, set_overlay_points, clear_overlay_points
-from .utils import viewSTQ
+from .utils import viewSTQ, viewMIQ
 
-__all__ = ['create_viewer', 'set_overlay_points', 'clear_overlay_points', 'viewSTQ']
-
-
-# def __getattr__(name):
-# 	if name in {'getTilesInContour', 'preparePatchesFromStrokes', 'visualizePatches'}:
-# 		from .utils import getTilesInContour, preparePatchesFromStrokes, visualizePatches
-
-# 		exports = {
-# 			'getTilesInContour': getTilesInContour,
-# 			'preparePatchesFromStrokes': preparePatchesFromStrokes,
-# 			'visualizePatches': visualizePatches,
-# 		}
-# 		return exports[name]
-# 	raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+__all__ = ['create_viewer', 'set_overlay_points', 'clear_overlay_points', 'viewSTQ', 'viewMIQ']
--- viewer/utils.py
+++ viewer/utils.py
@@ -92,3 +92,50 @@ def viewSTQ(dpath, imfname='image.ome.tiff', load_features=False, samples=None, 
         imgs = {s: os.path.join(dpath, s, imfname) for s in valid_samples}
 
         return create_viewer(valid_samples, imgs, height=height)[1]
+
+
+def viewMIQ(dpath, imfname='adjusted-all-channels.ome.tif', samples=None, height="800px",
+            mpp=0.2125, max_cells=10000):
+
+    """Creates a viewer for the given directory path containing sample subdirectories with image files.
+    Args:
+        dpath (str): The directory path containing sample subdirectories.
+        imfname (str): The expected filename of the image file within each sample subdirectory.
+        samples (list): A list of sample names to include in the viewer. If None, all valid samples will be included.
+        height (str): The height of the viewer.
+        mpp (float): Microns per pixel for the images.
+        max_cells (int): Maximum number of cells to display in the viewer.
+
+    Returns:
+        viewer: A viewer object created with the valid samples and their corresponding images.
+    """
+    samples_ = sorted([s for s in os.listdir(os.path.join(dpath)) if not s.startswith("pipeline") and os.path.isdir(os.path.join(dpath, s))])
+
+    if samples is not None:
+        samples_ = [s for s in samples_ if s in samples]
+
+    # Verify that each sample has image, otherwise filter out from the list
+    valid_samples = []
+    for s in samples_:
+        img_path = os.path.join(dpath, s, imfname)
+        if os.path.isfile(img_path):
+            valid_samples.append(s)
+        else:
+            print(f"Warning: Sample '{s}' does not have the expected image file '{imfname}' and will be skipped.")
+
+    if not valid_samples:
+        raise ValueError("No valid samples found with the expected image file.")
+
+
+    mif_images = {s: os.path.join(dpath, s, imfname) for s in valid_samples}
+    identity_matrices = {sample: './identity-matrix.csv' for sample in valid_samples}
+
+    xenium_bundle_paths = {sample: f"{dpath}/{sample}" for sample in valid_samples}
+
+    drawings = create_viewer(valid_samples, mif_images, height=height,
+                            xenium_mpp=mpp, max_cells=max_cells,
+                            matrices=identity_matrices,
+                            xenium_bundle_paths=xenium_bundle_paths,
+                        )[1]
+
+    return drawings
