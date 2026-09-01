71065eb0628d2dd9d093eea234420118ad747ccc
Sergii Domanskyi <Sergii.Domanskyi@jax.org>
2026-09-01 08:46:52 -0400

    faster local rep

--- dianne-core/dianne_core/__init__.py
+++ dianne-core/dianne_core/__init__.py
@@ -1,4 +1,4 @@
 from .combineCDF import getDiscreteCombinedCDFofAllFeatures as PCMA
-from .core import loadAd, preparePatchesWSI, getPatchRepresentation, inferProbFast, trainClassifier, loadDataAndPreparePatches
+from .core import loadAd, preparePatchesWSI, getPatchRepresentationParallel as getPatchRepresentation, inferProbFast, trainClassifier, loadDataAndPreparePatches
 
 __version__ = "0.1.0"
